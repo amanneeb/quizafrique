439003c33c6c8a32ef138fefa1c6a68c7639d1eb
Anaïs Mannée <anaismannee@gmail.com>
2022-08-26 11:28:55 -0400

    lien index/quiz

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 */
 
 function ouvrirQuiz(){
-    window.open(".../quizafrique/html/quiz.html")
+    window.open("./html/quiz.html")
 };
 
 document.querySelector(".btnQuiz").addEventListener("click", function(){
