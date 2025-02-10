cff59e224c4bc9fcbd5579b2288427d6198604b7
mbanais <anaismannee@gmail.com>
2025-02-10 12:45:02 -0500

    modification du fichier vite.config.js

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/quizafrique/',
+  base: '/quizafrique/vue-project/',
   plugins: [
     vue(),
   ],
